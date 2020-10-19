3fcb76c8bb358a1b4c715fa5d914357d4ca93639
sinhmaivan97 <sinhmaivan97@gmail.com>
2020-10-19 15:31:38 +0700

    priview screen app

--- app/src/main/java/com/teamwork/businessguideprofortiktok/src/resource/fragments/introduce_fragment/introduce_fragment3/IntroduceFragment3.java
+++ app/src/main/java/com/teamwork/businessguideprofortiktok/src/resource/fragments/introduce_fragment/introduce_fragment3/IntroduceFragment3.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +16,20 @@ import com.teamwork.businessguideprofortiktok.R;
 
 public class IntroduceFragment3 extends Fragment {
     private View view;
+    private EditText EdittextEmaiil, EdittextPass;
+    private Button ButtonGetCreate;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_introduce3,container,false);
+        view = inflater.inflate(R.layout.fragment_introduce3, container, false);
+        init();
         return view;
+    }
+
+    private void init() {
+        EdittextEmaiil = view.findViewById(R.id.email);
+        EdittextPass = view.findViewById(R.id.pass);
+        ButtonGetCreate = view.findViewById(R.id.btn_get_create);
     }
 }
