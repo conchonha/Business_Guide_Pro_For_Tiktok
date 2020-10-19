01e89c0b1b61344cd8814893de71fa8f98598196
sinhmaivan97 <sinhmaivan97@gmail.com>
2020-10-19 19:09:49 +0700

    push code

--- app/src/main/java/com/teamwork/businessguideprofortiktok/src/utils/Validations.java
+++ app/src/main/java/com/teamwork/businessguideprofortiktok/src/utils/Validations.java
@@ -1,4 +1,10 @@
 package com.teamwork.businessguideprofortiktok.src.utils;
 
+import android.service.autofill.RegexValidator;
+
 public class Validations {
+    public boolean validCellPhone(String number) {
+        return android.util.Patterns.PHONE.matcher(number).matches();
+    }
 }
+
